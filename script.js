0abf711e2cf9e844e4e9260f33d61f26993e8922
Kartik1913 <145954755+Kartik1913@users.noreply.github.com>
2024-07-04 00:32:55 +0530

    script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const wrapperRect = wrapper.getBoundingClientRect();
 const noBtnRect = noBtn.getBoundingClientRect();
 yesBtn.addEventListener('click', () => {
     question.innerHTML = 'I Love You Too :)';
+    wrapper.classlist.toggle('fa-star');
 });
 noBtn.addEventListener('mouseover', () => {
     const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
