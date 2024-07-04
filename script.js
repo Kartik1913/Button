f603c21c3bc3812b672654b75c41575bae4128b1
Kartik1913 <145954755+Kartik1913@users.noreply.github.com>
2024-07-04 16:09:15 +0530

    script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const noBtnRect = noBtn.getBoundingClientRect();
 yesBtn.addEventListener('click', () => {
     question.innerHTML = 'I Love You Too :)';
     icon.className = 'icon fa-solid fa-heart'; 
-    changeBackgroundImage('url("path_to_your_image.jpg")');
+    changeBackgroundImage('url("https://www.google.com/search?client=ms-android-vivo&sca_esv=e197cb97d6e85975&sca_upv=1&q=best+love+image&udm=2&fbs=AEQNm0AeMNWKf4PpcKMI-eSa16lJoRPMIuyspCxWO6iZW9F1Nu5UXlEfGU2YX1CrW9Nmm9Q3JIJZUqyMsLxos5tPU_UnqJ9Yac9VVJRGWfC4j5Vo8iVdmp0yHcykFiMIS9jUiQbN_U_vVDhXuiv9WtRFP3w1xyEfuag4hZCMtDiWR5yvDhV6r1V8aWdr2pDV3Csi4oLpFhod-EYWrnzWwrB0T8SfNvjCyg&sa=X&ved=2ahUKEwiwm9yCmI2HAxVv8DgGHVH9CT0QtKgLegQIGBAB&biw=360&bih=654&dpr=3#vhid=0C2FONzsCqeCXM&vssid=mosaic")');
 });
 noBtn.addEventListener('mouseover', () => {
     const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
